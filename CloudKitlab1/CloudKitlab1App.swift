9def557854c54f0ec0cbcbcf5d83509afd3e7e04
sara <sara.ahumidi4@gmail.com>
2022-12-11 11:01:58 +0300

    Initial Commit

---
+++ CloudKitlab1/CloudKitlab1App.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitlab1App.swift
+//  CloudKitlab1
+//
+//  Created by Sara Alhumidi on 17/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitlab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
